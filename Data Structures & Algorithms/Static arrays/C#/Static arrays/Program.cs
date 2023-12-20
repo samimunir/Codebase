dc67de06de8dc9a094b0bcbdcb34328746e657be
Sami Munir <smunir01@outlook.com>
2023-12-19 21:34:32 -0500

    adding Static Arrays - C#

---
+++ Data Structures & Algorithms/Static arrays/C#/Static arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+    Sami Munir
+    December 19th, 2023
+    Data Structures & Algorithms
+    - Static Arrays: C#
+*/
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("\nStatic Arrays - C#");
+        Console.WriteLine("------------------");
+    }
+}
--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,12 @@ Sami Munir | Rutgers University | Codebase - ReadMe.md
 ## Directory
 ### 1. Projects
 * Fun with JS - *HTML, CSS, Javascript*
+### 2. Complete 2023 Web Developer Bootcamp
+* HTML
+* CSS
+* Javascript
+* jQuery
+* Node.js
+### 3. Data Structures & Algorithms
+* Static arrays
+    * C#
