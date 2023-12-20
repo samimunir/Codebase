42bf30e62505f1b73cfb958b2bc6e458ff9e5638
Sami Munir <smunir01@outlook.com>
2023-12-19 20:13:26 -0500

    adding & chaining animations to HTML elements using jQuery

--- Complete 2023 Web Developer Bootcamp/jQuery/script.js
+++ Complete 2023 Web Developer Bootcamp/jQuery/script.js
@@ -2,29 +2,43 @@ $(document).ready(function() {
     $('body').css('background', '#262626');
 });
 
-$('h1').addClass('big-title margin-left');
+// $('h1').addClass('big-title margin-left');
 
-$('h1').text('Goodbye');
+// $('h1').text('Goodbye');
 
 // .html('');
 
 // $('img').attr('src');
 // $('a').attr('href', 'https://www.yahoo.com');
 
-$('h1').click(function() {
-    alert('hello');
-});
+// $('h1').click(function() {
+//     alert('hello');
+// });
 
-$('button').click(function() {
-    $('h1').css('color', 'purple');
-});
+// $('button').click(function() {
+//     $('h1').css('color', 'purple');
+// });
 
-$('input').keypress(function(event) {
-    console.log(event.key);
-});
+// $('input').keypress(function(event) {
+//     console.log(event.key);
+// });
 
-$('h1').on('mouseover', function() {
-    $('h1').css('color', '#0099ff');
-});
+// $('h1').on('mouseover', function() {
+//     $('h1').css('color', '#0099ff');
+// });
 
-$('h1').before('<button>New Button</button>');
+// $('h1').before('<button>New Button</button>');
+
+// $('button').on('click', function() {
+//     $('h1').fadeToggle();
+// });
+
+// $('button').on('click', function() {
+//     $('h1').animate({
+//         opacity: 0.5 // must be a number-valued CSS property
+//     });
+// });
+
+$('button').on('click', function() {
+    $('h1').slideUp().slideDown().animate({opacity: 0.5});
+});
