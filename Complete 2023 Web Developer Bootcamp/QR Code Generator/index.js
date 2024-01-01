886ef4653a7906c0950a73b7b509c9593ba44573
Sami Munir <smunir01@outlook.com>
2024-01-01 01:37:09 -0500

    adding functionality to enter input url

--- Complete 2023 Web Developer Bootcamp/QR Code Generator/index.js
+++ Complete 2023 Web Developer Bootcamp/QR Code Generator/index.js
@@ -1,1 +1,20 @@
-import requirer from 'inquirer';
+import inquirer from 'inquirer';
+
+inquirer
+    .prompt([
+        {
+            message: "Type in your URL: ",
+            name: "URL",
+        }
+    ])
+    .then((answers) => {
+        // console.log(answers);
+        const url = answers.URL;
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // prompt couldn't be rendered in the current environment
+        } else {
+            // something else went wrong
+        }
+    });
