441c3f7708024faa175936f657bb5fd7d65327ea
Sami Munir <smunir01@outlook.com>
2024-01-01 02:54:23 -0500

    adding /about & /contact routes

--- Complete 2023 Web Developer Bootcamp/Express/Backend/index.js
+++ Complete 2023 Web Developer Bootcamp/Express/Backend/index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello, World!</h1>');
 });
 
+app.get('/about', (req, res) => {
+    res.send('<h1>About Me</h1>');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact Me</h1>');
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
