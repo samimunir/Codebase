367d9a68a9cb3e77d2a350df063234fdf6b2c925
Sami Munir <smunir01@outlook.com>
2024-01-09 13:08:48 -0500

    adding dynamic message functionality based on day of week

--- Complete 2023 Web Developer Bootcamp/EJS/4.0 EJS/index.js
+++ Complete 2023 Web Developer Bootcamp/EJS/4.0 EJS/index.js
@@ -4,7 +4,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {dayType: 'a weekday', advice: 'it is time to work hard'});
+    const today = new Date();
+    const day = today.getDay();
+    let type = 'a weekday';
+    let adv = 'it is time to work hard';
+    if (day === 0 || day === 6) {
+        type = 'the weekend';
+        adv = 'it is time to have some fun!';
+    }
+    res.render('index.ejs', 
+    {
+        dayType: type, 
+        advice: adv,
+    });
 });
 
 app.listen(port, () => {
