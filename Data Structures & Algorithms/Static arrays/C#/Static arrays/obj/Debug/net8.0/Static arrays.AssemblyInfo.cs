8cbf44fcaad9eb6c9cc8e79fa72b513b25440080
Sami Munir <smunir01@outlook.com>
2024-01-09 12:34:19 -0500

    DS Algo dir update

--- Data Structures & Algorithms/Static arrays/C#/Static arrays/obj/Debug/net8.0/Static arrays.AssemblyInfo.cs
+++ Data Structures & Algorithms/Static arrays/C#/Static arrays/obj/Debug/net8.0/Static arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Static arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5600cc0ad1e131273911d24706ecd47de379db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Static arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Static arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
