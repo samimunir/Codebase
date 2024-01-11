8a54605866497f622891ff8f29880e9fb4f4dadc
Sami Munir <smunir01@outlook.com>
2024-01-11 15:16:09 -0500

    learning programming basics with C++

--- C++ From Beginner to Expert/ReadMe.md
+++ C++ From Beginner to Expert/ReadMe.md
@@ -1,2 +1,4 @@
 Sami Munir | Rutgers University | smunir01@outlook.com
 # C++ From Beginner to Expert
+## Course Directory
+### 1. Section 1 - Basics
---
+++ C++ From Beginner to Expert/Section 1 - Basics/ProgrammingBasics.cpp
@@ -0,0 +1,13 @@
+/*
+    IOstream stands for input-output stream
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+        cout steands for console-output
+    */
+    cout << "This is a test...";
+    return 0;
+}
