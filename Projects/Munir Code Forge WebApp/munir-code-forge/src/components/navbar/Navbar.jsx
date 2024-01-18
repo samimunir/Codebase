217377d681dd017cf341f05bc957b8dabb33534c
Sami Munir <smunir01@outlook.com>
2024-01-18 00:23:33 -0500

    restarting Navbar component

--- Projects/Munir Code Forge WebApp/munir-code-forge/src/components/navbar/Navbar.jsx
+++ Projects/Munir Code Forge WebApp/munir-code-forge/src/components/navbar/Navbar.jsx
@@ -4,18 +4,15 @@ import Logo from '../../assets/Munir Code Forge Logo.png';
 function Navbar() {
     return (
         <div className='Navbar'>
-            <div className='navbar-logo'>
+            <div className='navbar-logo-title'>
                 <img src={Logo} alt='logo'/>
                 <h1>Munir Code Forge</h1>
             </div>
             <div className='navbar-list'>
                 <a href='/'>Home</a>
-                <a href='/'>Our Work</a>
                 <a href='/'>About</a>
+                <a href='/'>Our Work</a>
                 <a href='/'>Contact</a>
-            </div>
-            <div className='navbar-connect'>
-                <button className='navbar-connect-btn'>Let's Connect</button>
             </div>
         </div>
     );
