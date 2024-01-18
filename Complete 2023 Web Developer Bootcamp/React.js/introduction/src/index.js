b40db64a7fa757560987aad4536e17c39b688dcd
Sami Munir <smunir01@outlook.com>
2024-01-18 01:11:06 -0500

    learning JSX expressions & template literals

--- Complete 2023 Web Developer Bootcamp/React.js/introduction/src/index.js
+++ Complete 2023 Web Developer Bootcamp/React.js/introduction/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const name = 'Sami M.';
+
 ReactDOM.render(
   <div>
-    <h1>Hello JSX & React.js</h1>
+    <h1>Hello, {name}</h1>
     <p>This is a paragraph...</p>
   </div>, 
 document.getElementById('root'));
