03a52702ba3cd44decc1392bce768393a4916dcb
Sami Munir <smunir01@outlook.com>
2024-01-18 19:59:57 -0500

    adding Footer component

--- Projects/Keeper App/keeper-react-app/src/components/App.jsx
+++ Projects/Keeper App/keeper-react-app/src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
     return (
-        <Header/>
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
     );
 }
 
