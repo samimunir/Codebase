1c2b860e72a17d4008104dae91c705c9557083bf
Sami Munir <smunir01@outlook.com>
2024-01-18 19:21:58 -0500

    adding HeroImg component

--- Complete 2023 Web Developer Bootcamp/React.js/learning-react-components/src/App.js
+++ Complete 2023 Web Developer Bootcamp/React.js/learning-react-components/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Heading from './components/heading/Heading';
 import Navbar from './components/navbar/Navbar';
+import HeroImg from './components/hero-img/Hero-img';
 
 function App() {
   return (
     <div className="App">
       <Heading/>
       <Navbar/>
+      <HeroImg/>
     </div>
   );
 }
---
+++ Complete 2023 Web Developer Bootcamp/React.js/learning-react-components/src/components/hero-img/Hero-img.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Hero-img.css';
+
+function HeroImg() {
+    return (
+        <div className='Hero-img'>
+            <div className='Hero-img-row1'>
+                <div className='Hero-img-row1-aside'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quisquam esse nobis doloribus eligendi ullam nam repudiandae voluptas iure ut labore alias excepturi quia sed ipsam, vero, earum nihil facere!
+                    </p>
+                </div>
+                <div className='Hero-img-row1-main'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id harum corrupti sint odio, sunt ut, omnis velit, sapiente a quod optio voluptate? Autem corporis minus quibusdam magnam, obcaecati exercitationem ab!
+                        Laudantium, quibusdam accusamus. Optio, impedit omnis incidunt dolorum dignissimos, harum rem ex nulla ab sed neque asperiores modi delectus laboriosam quia quo ipsam vero, facilis sapiente vel cupiditate. Ea, nostrum?
+                        Neque quisquam esse ad eligendi sequi porro consectetur, natus fugiat alias obcaecati aspernatur sapiente maiores libero hic similique exercitationem maxime odit inventore tempore adipisci ipsa. Impedit vero dolorum officiis dolore?
+                    </p>
+                </div>
+            </div>
+            <div className='Hero-img-row2'>
+                <div className='Hero-img-row2-col'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quisquam esse nobis doloribus eligendi ullam nam repudiandae voluptas iure ut labore alias excepturi quia sed ipsam, vero, earum nihil facere!
+                    </p>
+                </div>
+                <div className='Hero-img-row2-col'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quisquam esse nobis doloribus eligendi ullam nam repudiandae voluptas iure ut labore alias excepturi quia sed ipsam, vero, earum nihil facere!
+                    </p>
+                </div>
+                <div className='Hero-img-row2-col'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quisquam esse nobis doloribus eligendi ullam nam repudiandae voluptas iure ut labore alias excepturi quia sed ipsam, vero, earum nihil facere!
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeroImg;
