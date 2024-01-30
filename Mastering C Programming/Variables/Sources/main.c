8aaba4bc57c8460588c88d9847c345a177a65719
Sami Munir <smunir01@outlook.com>
2024-01-30 05:08:27 -0500

    learning variables with C

---
+++ Mastering C Programming/Variables/Sources/main.c
@@ -0,0 +1,16 @@
+/*
+    Sami Munir
+    January 30th, 2024
+    Variables
+    Mastering C Programming
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    char my_name[] = "Sami M";
+    printf("my_name: %s\n", my_name);
+    int my_age = 22;
+    printf("my_age: %d\n", my_age);
+    return EXIT_SUCCESS;
+}
