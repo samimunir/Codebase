14cd9d45d29ef09df235975ee20af5b3badd72ff
Sami Munir <smunir01@outlook.com>
2024-01-30 04:37:52 -0500

    Question 4 complete

--- Rutgers/CS210/Homework 1/homework_one.py
+++ Rutgers/CS210/Homework 1/homework_one.py
@@ -39,3 +39,32 @@ print('\tresult:', result)
 # Write a function called decreasing_digits to count all integers from 1
 #   to n (inclusive) that have all digits in decreasing order, where n is
 #   is given as a parameter.
+
+# Question 4 - stats
+# Write a function called stats to return the mean and median of a list of
+#   integers given as a parameter.
+import math
+def stats(ints):
+    sortedInts = sorted(ints)
+    total = 0
+    for i in range(len(sortedInts)):
+        total += sortedInts[i]
+    average = total / len(sortedInts)
+    if (len(sortedInts) % 2 == 0):
+        mid = math.floor(len(sortedInts) / 2)
+        a = sortedInts[mid]
+        b = sortedInts[mid - 1]
+        median = (a + b) / 2
+    else:
+        mid = math.floor(len(sortedInts) / 2)
+        median = sortedInts[mid]
+    return {'mean': average, 'median': median}
+
+# <--- Testing function stats(ints) --->
+result = stats([15, 4, 10, 2])
+print('\nstats([15, 4, 10, 2]):')
+print('\tresult:', result)
+
+result = stats([-1, 12, -63, 17, 2, 11, 94, -47, 11])
+print('\nstats([-1, 12, -63, 17, 2, 11, 94, -47, 11]):')
+print('\tresult:', result)
