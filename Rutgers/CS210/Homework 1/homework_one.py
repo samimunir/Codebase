ea96dd3288698c420520dfae70e86866e2fc7ce7
Sami Munir <smunir01@outlook.com>
2024-01-30 03:49:01 -0500

    Question 1 complete

--- Rutgers/CS210/Homework 1/homework_one.py
+++ Rutgers/CS210/Homework 1/homework_one.py
@@ -6,6 +6,30 @@
 # Question 1 - primes
 # Write a function to return all the prime numbers less than n, where
 #   n is a parameter (assume n > 2).
+def primes(n):
+    primes = []
+    for num in range(2, n):
+        isPrime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(num)
+    return primes
+
+# <--- Testing function primes(n) --->
+result = primes(10)
+print('\nprimes(10):')
+print('\tresult:', result)
+
+result = primes(3)
+print('\nprimes(3):')
+print('\tresult:', result)
+
+result = primes(100)
+print('\nprimes(100):')
+print('\tresult:', result)
 
 # Question 2 - factor
 # Write a function to return all the prime factors of n, where n is a
