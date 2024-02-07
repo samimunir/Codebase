dea77372e7c70f20129770db87daa296fc58309a
Sami Munir <smunir01@outlook.com>
2024-02-07 07:15:32 -0500

    learning Structures in C

---
+++ Mastering C Programming/Structs/source/main.c
@@ -0,0 +1,20 @@
+/*
+    Sami Munir
+    February 7th, 2024
+    Structures
+    Mastering C Programming
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+};
+
+int main(int argc, char* argv[]) {
+    // insert code here...
+    return EXIT_SUCCESS;
+}
