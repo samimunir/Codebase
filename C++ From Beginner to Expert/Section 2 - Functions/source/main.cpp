dd2be5d77633c8cda18f5fcb65101ace4e7dc1f7
Sami Munir <smunir01@outlook.com>
2024-02-08 05:40:42 -0500

    more with functions in C++

--- C++ From Beginner to Expert/Section 2 - Functions/source/main.cpp
+++ C++ From Beginner to Expert/Section 2 - Functions/source/main.cpp
@@ -9,9 +9,26 @@
 #include <iostream>
 using namespace std;
 
-int globalVariable;
+int globalVariable = 7;
+
+void printHello() {
+    cout << "Hello World!" << endl;
+}
+
+int sum(int num1, int num2) {
+    return (num1 + num2);
+}
 
 int main() {
-    int localVariable;
-    return 0;
+    int localVariable = 10;
+    for (int i = 0; i < globalVariable; i++) {
+        cout << "Num " << (i + 1) << ": " << (localVariable * globalVariable) << endl;
+        localVariable += i;
+    }
+
+    printHello();
+
+    cout << "\n2 + 3 = " << sum(2, 3) << endl;
+    
+    return EXIT_SUCCESS;
 }
