d51750818dc90c9f12a4b771e6d67b3e29858363
Sami Munir <smunir01@outlook.com>
2024-02-12 12:28:43 -0500

    CS210 homework1: primes() & factor() functions impl & testing complete

--- ReadMe.md
+++ ReadMe.md
@@ -35,6 +35,8 @@ Sami Munir | Rutgers University | Codebase - ReadMe.md
 ### Rutgers
 * CS210 - Data Management for Data Science
     * Homework 1
+        * Q1 - primes(n)
+        * Q2 - factor(n)
 * CS416 - Operating System Design
     * Project 1
         * stack.c
--- Rutgers/CS210/Homework 1/homework_one.py
+++ Rutgers/CS210/Homework 1/homework_one.py
@@ -19,21 +19,46 @@ def primes(n):
     return primes
 
 # <--- Testing function primes(n) --->
+print('\n<--- Testing function primes(n) --->')
 result = primes(10)
-print('\nprimes(10):')
+print('primes(10):')
 print('\tresult:', result)
 
 result = primes(3)
-print('\nprimes(3):')
+print('primes(3):')
 print('\tresult:', result)
 
 result = primes(100)
-print('\nprimes(100):')
+print('primes(100):')
 print('\tresult:', result)
 
 # Question 2 - factor
 # Write a function to return all the prime factors of n, where n is a
 #   parameter.
+def factor(n):
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors
+
+# <--- Testing function factor(n) --->
+print('\n<--- Testing function factor(n) --->')
+result = factor(10)
+print('factor(10):')
+print('\tresult:', result)
+result = factor(12)
+print('factor(12):')
+print('\tresult:', result)
+result = factor(35)
+print('factor(35):')
+print('\tresult:', result)
+result = factor(40)
+print('factor(40):')
+print('\tresult:', result)
 
 # Question 3 - decreasing_digits
 # Write a function called decreasing_digits to count all integers from 1
@@ -61,10 +86,11 @@ def stats(ints):
     return {'mean': average, 'median': median}
 
 # <--- Testing function stats(ints) --->
+print('\n<--- Testing function stats(ints) --->')
 result = stats([15, 4, 10, 2])
-print('\nstats([15, 4, 10, 2]):')
+print('stats([15, 4, 10, 2]):')
 print('\tresult:', result)
 
 result = stats([-1, 12, -63, 17, 2, 11, 94, -47, 11])
-print('\nstats([-1, 12, -63, 17, 2, 11, 94, -47, 11]):')
+print('stats([-1, 12, -63, 17, 2, 11, 94, -47, 11]):')
 print('\tresult:', result)
