538478cc611461939ef4ed996fcf2f92154dce07
Sami Munir <smunir01@outlook.com>
2024-02-15 11:48:58 -0500

    starting C++: Programming Basics

---
+++ C++ From Beginner to Expert/Programming Basics/source/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Sami Munir
+    February 15th, 2024
+    C++ From Beginner to Expert
+    Programming Basics
+    - main.cpp
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Programming Basics." << endl;
+
+    return EXIT_SUCCESS;
+}
