cd22aba02f81882f482ff287cfc538d12108872e
Sami Munir <smunir01@outlook.com>
2024-02-27 01:58:22 -0500

    handling possible arguments case to main()

--- Rutgers/CS416/Sample Programs/source/get_context.c
+++ Rutgers/CS416/Sample Programs/source/get_context.c
@@ -6,3 +6,21 @@
     - Sample Program
         - get_context.c
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <ucontext.h>
+
+#define STACK_SIZE SIGSTKSZ
+
+int main(int argc, char* argvp[]) {
+    ucontext_t cctx, ncctx;
+
+    if (argc != 1) {
+        printf("--<BAD USAGE: no arguments expected>--\n");
+        exit(1);
+    }
+}
