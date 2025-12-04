0ef955a7ba391fd4a977b39b9a3a2dce1903b694
Sami Munir <samidmunir@outlook.com>
2025-12-04 04:03:24 -0500

    implementing auth-router & main server.js

---
+++ Projects/mern-auth/services/auth/src/routes/auth.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  register,
+  login,
+  refresh,
+  logout,
+  me,
+} from "../controllers/auth.controller.js";
+import requireAuth from "../middleware/auth.middleware.js";
+
+const authRouter = express.Router();
+
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/refresh", refresh);
+authRouter.post("/logout", logout);
+authRouter.get("/me", requireAuth, me);
+
+export default authRouter;
---
+++ Projects/mern-auth/services/auth/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/auth.routes.js";
+import ENV from "./config/env.js";
+
+const app = express();
+
+const PORT = ENV.PORT;
+
+connectDB();
+
+app.use(morgan());
+
+app.use(cors({ origin: ENV.CLIENT, credentials: true }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/health", (req, res) => {
+  return res
+    .status(200)
+    .json({ ok: true, source: "<api.auth>", message: "/api/auth is live" });
+});
+
+app.use("/", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`✅ <api/gateway> live on localhost:${PORT}`);
+});
