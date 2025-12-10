1371a84f4bdf2a1e345417a8fe6aac28bfb0ee9d
Sami Munir <samidmunir@outlook.com>
2025-12-10 00:02:26 -0500

    variables in Rust

---
+++ Rust Programming/variables/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Variables in Rust");
+    println!("-------------------------");
+
+    /*
+        i32 represents 32-bit Integer (4 bytes)
+    */
+    let fav_num = 7;
+    println!("\nfav_num: {}", fav_num);
+
+    let a: i8 = 2;
+    let b: i8 = 3;
+    let sum = a + b;
+    println!("\na: {}, b: {}", a, b);
+    println!("sum ({} + {}): {}", a, b, sum);
+}
